fc41cb1de267911900e365e592e36119ce08c0f7
RDPW1SH <rafael10ambrosio@gmail.com>
2024-11-05 15:56:52 +0000

    middleware problems

--- e-votar/middleware.js
+++ e-votar/middleware.js
@@ -12,10 +12,7 @@ export default withAuth(function middleware(req) {
     if(req.nextUrl.pathname.startsWith("/votacoes/criar") && !token) {
         return NextResponse.rewrite(new URL('/login', req.url));
     } 
-    
-    if(req.nextUrl.pathname.startsWith("/login") && token || req.nextUrl.pathname.startsWith("/register") && token ) {
-        return NextResponse.rewrite(new URL('/', req.url));
-    }  
+
         
     return NextResponse.next();
     
@@ -26,5 +23,5 @@ export default withAuth(function middleware(req) {
 
 })
 
-export const config = {matcher: ["/votacoes/criar", "/login", "/register"]};
+export const config = {matcher: ["/votacoes/criar"]};
 
